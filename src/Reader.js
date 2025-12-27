564284630eba00c1786844833a00d29c9492cc25
Ahmed Baruwa <aobaruwa@yahoo.com>
2025-12-27 01:31:04 -0800

    add page extractor

--- src/Reader.js
+++ src/Reader.js
@@ -1,11 +1,33 @@
 import { pdf } from "pdf-to-img";
+import { promises as fs } from 'node:fs';
 
 // Count the number of pages in PDF document
 export async function checkImgs(fPath, outDir){
-    const document = await pdf(fPath, { scale: 3 });
+    const document = await pdf(fPath, { scale: 1 });
     return document.length;
 }
 
 // TODO: extract pages of PDF document
-
+export async function extractPages(fPath, outDir){
+    
+    let counter = 1;
+    const document = await pdf(fPath, { scale: 1 });
+    // check existence of output directory
+    try {
+            await fs.access(outDir, fs.constants.F_OK);
+            console.log(`${outDir} exists`);
+    } catch (err) {
+        console.log(`${outDir} doesn't exist - creating directory ...`);
+        await fs.mkdir(outDir, { recursive: true });
+    }
+    // save each page as image
+    for await (const img of document) {
+        console.log(`Saving page ${counter} as image...`);
+        console.log(`Output path: ${outDir}/page${counter}.png`); 
+        
+        await fs.writeFile(`${outDir}/page${counter}.png`, img);
+        counter ++;
+    }
+    return document;
+}
 // Check if a page has highlights
--- src/test/pages.test.js
+++ src/test/pages.test.js
@@ -1,8 +1,7 @@
 import { describe, expect, test} from "vitest";
 import { max, mul } from "../simple";
-import {checkImgs} from '../Reader';
-// import { PdfProcessor } from "../Reader";
-
+import { checkImgs, extractPages } from '../Reader';
+import { promises as fs } from 'node:fs';   
 describe('max', () => {
     test('should return the maximum value in an array', () => {
         expect(max(1, 3)).toBe(3);
@@ -19,9 +18,18 @@ describe('mul', () => {
 });
 describe('pdfReader', () => {
     test('Should read pdf file and report the number of pages', async() => {
-        const pdfFilePath = "../Reader/test.pdf"; // Path to test pdf file
+        const pdfFilePath = "/Users/ahmedbaruwa/Desktop/Contrib/Salma-Abugideri-Mohamed-Hag-Magid_Before-You-Tie-the-Knot-A-Guide-for-Couples__E_1351_B-odd7ih.pdf"; // Path to test pdf file
         const outDir = "Output"; // Output directory 
         const n_Pages = await checkImgs(pdfFilePath, outDir);
         expect(n_Pages).toBe(350);
     });
+});
+describe('pageExtraction', () => {
+    test('Should extract pages from pdf file and save as images', async() => {
+        const pdfFilePath = "/Users/ahmedbaruwa/Desktop/Contrib/Salma-Abugideri-Mohamed-Hag-Magid_Before-You-Tie-the-Knot-A-Guide-for-Couples__E_1351_B-odd7ih.pdf"; // Path to test pdf file
+        const outDir = "/Users/ahmedbaruwa/Desktop/Contrib/Pdf-Highlights-Finder/src/Output"; // Output directory 
+        const document = await extractPages(pdfFilePath, outDir);
+        const numImagesInOutputDir = fs.readdir(outDir).length; //.filter(file => /\.(png|jpg|jpeg|gif)$/i.test(file)).length;
+        expect(document.length).toBe(350);
+    }, 86400);
 });
